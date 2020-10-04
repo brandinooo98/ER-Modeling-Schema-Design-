ed0f483691a5791e746b92956050f4a380c606de
Alzomaili <48036688+Alzomaili@users.noreply.github.com>
2020-10-04 18:13:00 -0500

    query1 updated

--- query1.sql
+++ query1.sql
@@ -1,6 +1,10 @@
 /*
 Find the number of users in the database.
 */
-SELECT 
-(SELECT COUNT(*) FROM Seller) +
-(SELECT COUNT(*) FROM Bidder);
+SELECT COUNT(*)
+FROM
+(
+    SELECT userID FROM Seller
+    UNION
+    SELECT userID FROM Bidder
+);
