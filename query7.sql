715203a2b8221324b506a638bd380e25a9f67bc6
James Ma <jamesma100@gmail.com>
2020-10-04 21:13:25 -0500

    finish query7

--- query7.sql
+++ query7.sql
@@ -1,10 +1,10 @@
 /*
 Find the number of categories that include at least one item with a bid of more than $100.
 */
-SELECT COUNT(Category.categoryID)
-FROM Category, LineItem, Bid
+SELECT COUNT(DISTINCT Category.categoryID)
+FROM Category, LineItem
 WHERE Category.categoryID = LineItem.categoryID
-AND LineItem.itemID IN 
-    (SELECT Bid.itemID
-    FROM Bid
-    WHERE Bid.amount > 100);
+AND LineItem.itemID IN
+(SELECT Bid.itemID
+FROM Bid
+WHERE CAST(Bid.amount AS DOUBLE) > 100.);
