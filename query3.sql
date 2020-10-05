ccbf302185f7ab3d5f93ec19b3be1e77c281e8f3
Alzomaili <48036688+Alzomaili@users.noreply.github.com>
2020-10-04 19:23:42 -0500

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -2,5 +2,10 @@
 Find the number of auctions belonging to exactly four categories.
 */
 
-SELECT COUNT(*)
-FROM lineItemp
+SELECT COUNT(itemID)
+FROM
+(
+    SELECT itemID, COUNT(categoryID) AS COUNT
+    FROM LineItem GROUP BY itemID
+)
+WHERE COUNT = 4;
